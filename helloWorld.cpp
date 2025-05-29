fd739b3aea72b3a55acc76a681982a1e341c67cb
Khadija Tahira <112818364+KhadijaKT@users.noreply.github.com>
2025-05-29 11:34:37 +0500

    Update helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,6 +1,13 @@
-#inlcude<iostream>
-using namespace std;
-int main(){
-  cout<<"Check my code!";
-  return 0;
+// Necessary header files for input output functions
+#include <iostream>
+/using namespace std;
+
+// main() function: where the execution of
+/ C++ program begins
+int main() {
+  
+    // This statement prints "Hello World"
+    cout << "Hello World";
+
+    return 0;
 }
