657a25c282a4dc82aa225c01d08fedae9514ffbc
Khadija Tahira <112818364+KhadijaKT@users.noreply.github.com>
2025-05-29 11:44:00 +0500

    without comments

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 using namespace std;
-
-// main() function: where the execution of
-// C++ program begins
 int main() {
-    // This statement printsThe failing job identified issues in three files: `helloWorld.cpp`, `arrays.java`, and `style.css`. Below are the "Hello World"
     cout << "Hello World";
-    return  solutions to address the errors:
-
----
-
-### **10;
+    return 0;
 }
